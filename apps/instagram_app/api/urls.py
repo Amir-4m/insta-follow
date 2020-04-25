d3b3cd44ca25613b0c20e38b925eea26eef3fd03
mujad <muhamad.alinejad@gmail.com>
2020-04-25 19:14:12 +0430

    add Like ApiView

--- apps/instagram_app/api/urls.py
+++ apps/instagram_app/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import InstaPageViewSet, LikedPageAPIVIEW
+from .views import InstaPageViewSet, LikedPageAPIVIEW, UserInquiryLikeAPIView
 
 urlpatterns = [
     path('user/liked/', LikedPageAPIVIEW.as_view(), name="liked"),
-
+    path('inquiries/like/', UserInquiryLikeAPIView.as_view(), name="user-inquiries-like"),
 ]
+
 router = routers.DefaultRouter()
 router.register('user/page', InstaPageViewSet, basename="InstaPage")
 
--- apps/instagram_app/api/views.py
+++ apps/instagram_app/api/views.py
@@ -1,12 +1,11 @@
 from rest_framework import status, mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import InstaPageSerializer, LikedPageSerializer
-from apps.instagram_app.models import InstaPage, UserPage
+from apps.instagram_app.models import InstaPage, UserPage, Order
 
 
 class InstaPageViewSet(mixins.CreateModelMixin,
@@ -45,3 +44,17 @@ class LikedPageAPIVIEW(APIView):
         if serializer.is_valid():
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInquiryLikeAPIView(APIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        page_id = request.data.get('page_id')
+        user_page = UserPage.objects.get(page_id=page_id)
+        valid_orders = Order.objects.all()[:10]
+        for order in valid_orders:
+            pass
+
+        return Response({'msg': True})
