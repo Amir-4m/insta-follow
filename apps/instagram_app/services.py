3aeaa6ebbb92176bb4c4a0009dc3cc3f24d88fdf
amir <amiri.amir30@yahoo.com>
2020-06-16 13:00:40 +0430

    login restrict implemented

--- apps/instagram_app/services.py
+++ apps/instagram_app/services.py
@@ -142,8 +142,8 @@ class InstagramAppService(object):
                 return instagram
 
             except Exception as e:
-                if InstagramAccount.objects.filter(is_enable=True).count() == 1:
-                    break
+                instagram_account.is_enable = False
+                instagram_account.save()
                 logger.error(f"logging in to instagram with account {instagram_account.username} got error: {e}")
         raise Exception("instagram login reached max retries !")
 
