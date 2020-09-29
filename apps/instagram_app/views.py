fa0c1823c5c3a0c81c7218e44cc88908100f8bb6
Amir <amiri.amir30@yahoo.com>
2020-09-29 11:29:48 +0330

    failed payment template added

--- apps/instagram_app/views.py
+++ apps/instagram_app/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
@@ -8,7 +9,11 @@ from apps.instagram_app.models import CoinPackageOrder
 class PaymentView(View):
     def get(self, request, *args, **kwargs):
         invoice_number = request.GET.get('service_reference')
-        purchase_verified = request.GET.get('purchase_verified')
+        purchase_verified = json.loads(request.GET.get('purchase_verified'))
+        if purchase_verified is True:
+            html = 'instagram_app/payment_done.html'
+        else:
+            html = 'instagram_app/payment_failed.html'
         try:
             order = CoinPackageOrder.objects.get(invoice_number=invoice_number)
         except CoinPackageOrder.DoesNotExist:
@@ -18,4 +23,4 @@ class PaymentView(View):
             "redirect_url": order.redirect_url,
             "purchase_verified": purchase_verified
         }
-        return render(request, 'instagram_app/payment_done.html', context)
+        return render(request, html, context)
