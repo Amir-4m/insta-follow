d80748b3c586c1a49fd7790f1e51dc6a450aeb82
Arash F <arash@hami.blue>
2021-02-02 17:38:46 +0330

    BUGFIX: account-private

--- apps/instagram_app/api/views.py
+++ apps/instagram_app/api/views.py
@@ -48,7 +48,7 @@ class PrivateAccount(views.APIView):
 
     def get(self, request, *args, **kwargs):
         page = request.auth['page']
-        orders = Order.objects.filter(owner=page, action__action_type=InstaAction.ACTION_FOLLOW, is_enable=True)
+        orders = Order.objects.filter(entity_id=page.instagram_user_id, action__action_type=InstaAction.ACTION_FOLLOW, is_enable=True)
         for order in orders:
             check_order_validity.delay(order.pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
