f4863a9580c8ae12fe5a9551fdc208f15d4a1d8c
Amir <amiri.amir30@yahoo.com>
2021-04-03 16:20:37 +0430

    minor bug fix

--- apps/instagram_app/admin.py
+++ apps/instagram_app/admin.py
@@ -80,6 +80,12 @@ class CoinPackageOrderModelAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(Comment)
 class CommentModelAdmin(admin.ModelAdmin):
