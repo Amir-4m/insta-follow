d5a221c687bcfb6e35738745ef6c3227ff0f57e7
Amir <amiri.amir30@yahoo.com>
2021-04-04 14:13:57 +0430

    purchase verify updated

--- apps/instagram_app/signals.py
+++ apps/instagram_app/signals.py
@@ -1,23 +1,22 @@
 import logging
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import InstaPage, Order
+from .models import CoinPackageOrder, CoinTransaction
 
 logger = logging.getLogger(__name__)
 
 
-# @receiver(post_save, sender=Order)
-# def order_receiver(sender, instance, created, raw=False, **kwargs):
-#     if created and not raw:
-#         action = instance.action.action_type
-#         media_url = instance.media_url or ''
-#         collect_order_link_info.delay(
-#             order_id=instance.id,
-#             action=action,
-#             link=instance.link,
-#             media_url=media_url,
-#         )
-#
+@receiver(pre_save, sender=CoinPackageOrder)
+def package_order_receiver(sender, instance, **kwargs):
+    if instance._b_is_paid is None and instance.is_paid is True:
+        coin_package = instance.coin_package
+        ct_amount = coin_package.amount if coin_package.amount_offer is None else coin_package.amount_offer
+        CoinTransaction.objects.create(
+            page=instance.page,
+            amount=ct_amount,
+            package=instance,
+            transaction_type=CoinTransaction.TYPE_PURCHASE,
+        )
 
 # @receiver(post_save, sender=InstaPage)
 # def insta_page_receiver(sender, instance, created, raw=False, **kwargs):
