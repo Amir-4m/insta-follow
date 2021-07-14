4118655e3d0c02a21317a90dc0936c2be87c9168
m.rezaei <m.rezaei@hami.blue>
2021-07-14 13:25:08 +0430

    swagger gift code api updated

--- apps/reward/swagger_schemas.py
+++ apps/reward/swagger_schemas.py
@@ -50,20 +50,11 @@ TAPSELL_REWARD_DOCS = openapi.Schema(
 
 GIFT_CODE_DOCS_RESPONSE = openapi.Schema(
     type=openapi.TYPE_OBJECT,
+    required=['code'],
     properties={
-        'page': openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            description='username of the page which got the reward'
-
-        ),
-        'amount': openapi.Schema(
+        'code': openapi.Schema(
             type=openapi.TYPE_INTEGER,
-            description='amount of the coins that rewarded to the page.'
-        ),
-        'rewarded': openapi.Schema(
-            type=openapi.TYPE_BOOLEAN,
-            description='status of the reward operation. this is true if page gets the reward successfully'
-        ),
-
+            description='gift code'
+        )
     }
 )
